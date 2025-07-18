8a61db376b1654551c3410ff8bbd24b3bcde9118
SakuraNene <skrnn0505@gmail.com>
2025-07-18 14:48:10 +0900

    ver1.0

---
+++ include/NeneIcecream/NeneIcecream.hpp
@@ -0,0 +1,55 @@
+#pragma once
+#include <iostream>
+#include <typeinfo>
+#include <utility>
+
+namespace dbg_detail {
+
+// yes/no テスト用
+template<class...> using void_t = void;
+
+// デフォルト false
+template<class T, class = void>
+struct is_streamable : std::false_type {};
+
+// os << std::declval<const T&>() が有効なら true
+template<class T>
+struct is_streamable<T, void_t<
+    decltype( std::declval<std::ostream&>() << std::declval<const T&>() )
+>> : std::true_type {};
+
+template<class T>
+inline constexpr bool is_streamable_v = is_streamable<T>::value;
+
+} // namespace dbg_detail
+
+namespace dbg {
+
+inline std::ostream*& out_stream() {
+    static std::ostream* s = &std::cout;
+    return s;
+}
+inline void set_stream(std::ostream& os) { out_stream() = &os; }
+
+template<class T>
+void print_value(std::ostream& os, T&& v) {
+    if (dbg_detail::is_streamable_v<T>) {
+        os << v;
+    } else {
+        os << "<" << typeid(T).name() << ">";
+    }
+}
+
+template<class T>
+T&& ic_impl(const char* expr_str, const char* file, int line, T&& value) {
+    std::ostream& os = *out_stream();
+    os << "[" << file << ":" << line << "] "
+       << expr_str << " = ";
+    print_value(os, value);
+    os << '\n';
+    return std::forward<T>(value);
+}
+
+} // namespace dbg
+
+#define nnic(expr) ::dbg::ic_impl(#expr, __FILE__, __LINE__, (expr))
---
+++ test.cpp
@@ -0,0 +1,7 @@
+#include <NeneIcecream.hpp>
+
+int main() {
+    int x = 42;
+    int y = 7;
+    int z = nnic(x + y);
+}
