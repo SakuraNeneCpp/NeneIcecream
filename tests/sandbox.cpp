9bc1f28b3c5994cb4354c7bfc588980bdac81256
SakuraNene <skrnn0505@gmail.com>
2025-07-23 14:58:53 +0900

    update

---
+++ tests/sandbox.cpp
@@ -0,0 +1,9 @@
+#include <NeneIcecream/NeneIcecream.hpp>
+
+int main(){
+    int a = 2;
+    int b = 3;
+    int c = ic(a + b);
+    ic(c);
+    return 0;
+}
--- tests/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <NeneIcecream/NeneIcecream.hpp>
-
-int main(){
-    int a = 2;
-    int b = 3;
-    int c = ic(a + b);
-    ic(c);
-    return 0;
-}
